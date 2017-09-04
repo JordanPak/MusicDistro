c60adc205166a5e9fa4eb11135850bcd34679b99
JordanPak <jpak@jordanpak.com>
2017-09-04 19:19:26 +0000

    Register arrangement cpt

--- includes/class-arrangement.php
+++ includes/class-arrangement.php
@@ -18,7 +18,7 @@ class MusicDistro_Arrangement_Handler {
      * @var string
      * @since 1.0.0
      */
-    const CPT_SLUG = MD_CPT_PREFIX . 'arrangement';
+    public $cpt_slug = MD_CPT_PREFIX . 'arrangement';
 
 
     /**
@@ -40,6 +40,74 @@ class MusicDistro_Arrangement_Handler {
      */
     public function register_cpt() {
 
+        $labels = array(
+            'name'                  => _x( 'Arrangements', 'Post Type General Name', 'musicdistro' ),
+            'singular_name'         => _x( 'Arrangement', 'Post Type Singular Name', 'musicdistro' ),
+            'menu_name'             => __( 'Arrangements', 'musicdistro' ),
+            'name_admin_bar'        => __( 'Arrangement', 'musicdistro' ),
+            'archives'              => __( 'Arrangement Archives', 'musicdistro' ),
+            'attributes'            => __( 'Arrangement Attributes', 'musicdistro' ),
+            'parent_item_colon'     => __( 'Parent Arrangement:', 'musicdistro' ),
+            'all_items'             => __( 'All Arrangements', 'musicdistro' ),
+            'add_new_item'          => __( 'Add New Arrangement', 'musicdistro' ),
+            'add_new'               => __( 'Add New', 'musicdistro' ),
+            'new_item'              => __( 'New Arrangement', 'musicdistro' ),
+            'edit_item'             => __( 'Edit Arrangement', 'musicdistro' ),
+            'update_item'           => __( 'Update Arrangement', 'musicdistro' ),
+            'view_item'             => __( 'View Arrangement', 'musicdistro' ),
+            'view_items'            => __( 'View Arrangements', 'musicdistro' ),
+            'search_items'          => __( 'Search Arrangement', 'musicdistro' ),
+            'not_found'             => __( 'Not found', 'musicdistro' ),
+            'not_found_in_trash'    => __( 'Not found in Trash', 'musicdistro' ),
+            'featured_image'        => __( 'Featured Image', 'musicdistro' ),
+            'set_featured_image'    => __( 'Set featured image', 'musicdistro' ),
+            'remove_featured_image' => __( 'Remove featured image', 'musicdistro' ),
+            'use_featured_image'    => __( 'Use as featured image', 'musicdistro' ),
+            'insert_into_item'      => __( 'Insert into arrangement', 'musicdistro' ),
+            'uploaded_to_this_item' => __( 'Uploaded to this arrangement', 'musicdistro' ),
+            'items_list'            => __( 'Arrangements list', 'musicdistro' ),
+            'items_list_navigation' => __( 'Arrangements list navigation', 'musicdistro' ),
+            'filter_items_list'     => __( 'Filter arrangements list', 'musicdistro' ),
+        );
+
+        $capabilities = array(
+            'edit_post'             => 'edit_post',
+            'read_post'             => 'read_post',
+            'delete_post'           => 'delete_post',
+            'edit_posts'            => 'edit_posts',
+            'edit_others_posts'     => 'edit_others_posts',
+            'publish_posts'         => 'publish_posts',
+            'read_private_posts'    => 'read_private_posts',
+        );
+
+
+        // CPT arguments, filterable if needed
+        $args = apply_filters( 'musicdistro_arrangement_args',
+            array(
+                'label'                 => __( 'Arrangement', 'musicdistro' ),
+                'description'           => __( 'A single arrangement for a band to use: Sheet music, tempo, recording, etc.', 'musicdistro' ),
+                'labels'                => $labels,
+                'supports'              => array( 'title', ),
+                // 'taxonomies'            => array( 'MD_BANDS_KILL', ' MD_ARRANGEMENT_TYPE_KILL' ),
+                'hierarchical'          => false,
+                'public'                => true,
+                'show_ui'               => true,
+                'show_in_menu'          => true,
+                'menu_position'         => 5,
+                'menu_icon'             => 'dashicons-download',
+                'show_in_admin_bar'     => true,
+                'show_in_nav_menus'     => false,
+                'can_export'            => true,
+                'has_archive'           => false,		
+                'exclude_from_search'   => true,
+                'publicly_queryable'    => true,
+                'rewrite'               => false,
+                'capabilities'          => $capabilities,
+                'show_in_rest'          => true,
+            )
+        );
+
+        register_post_type( $this->cpt_slug, $args );
     }
 
 }
