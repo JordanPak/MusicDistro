dba7b2845d6079d2fe3b75fddb527308bfc03bc1
JordanPak <jpak@jordanpak.com>
2017-09-04 15:28:15 +0000

    Start setting up main MusicDistro instance

--- musicdistro.php
+++ musicdistro.php
@@ -21,19 +21,87 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 
 /**
- * Define and grab our includes
+ * Main MusicDistro class.
+ *
+ * @since 0.1.0
  */
-$includes = array(
-	'functions',
-	'class-case-study',
-);
-foreach ( $includes as $include ) {
-	// require_once 'inc/' . $include . '.php';
+class MusicDistro {
+
+	/**
+	 * @var MusicDistro the instance of MusicDistro
+	 * @since 0.1.0
+	 */
+	private static $instance;
+
+
+	/**
+	 * @var object|MD_Roles
+	 * @since 0.1.0
+	 */
+	public $roles;
+
+
+
+	/**
+	 * Main MusicDistro instance.
+	 *
+	 * @since 0.1.0
+	 * @static
+	 * @see MusicDistro()
+	 * @return object|MusicDistro the instance of MusicDistro
+	 */
+	public static function instance() {
+
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof MusicDistro ) ) {
+
+			self::$instance = new MusicDistro;
+			self::$instance->setup_constants();
+
+			add_action( 'plugins_loaded', array( self::$instance, 'load_textdomain' ) );
+
+			self::$instance->includes();
+			self::$instance->roles = new MD_Roles();
+		}
+
+		return self::$instance;
+	}
+
+
+
+	/**
+	 * Throw error on object clone.
+	 *
+	 * The whole idea of the singleton design pattern is that there is a single
+	 * object therefore, we don't want the object to be cloned.
+	 *
+	 * @since 0.1.0
+	 * @access protected
+	 * @return void
+	 */
+	public function __clone() {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'musicdistro' ), '0.1.0' );
+	}
+
+
+	
+	/**
+	 * Setup plugin constants.
+	 *
+	 * @since 0.1.0
+	 * @return void
+	 */
+	private function setup_constants() {
+
+		// plugin version
+		define( 'MD_VERSION', '0.1.0' );
+
+		// plugin folder path
+		define( 'MD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+		// plugin folder URL
+		define( 'MD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+		// plugin root file
+		define( 'MD_PLUGIN_FILE', __FILE__ );
+	}
 }
-// require_once 'class-main.php';
-
-
-/**
- * Start everything up
- */
-// MusicDistro::instance();
