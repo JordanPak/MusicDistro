258dc483590f72dd709dab63c56a8b90e7c34d2e
JordanPak <jpak@jordanpak.com>
2017-09-04 18:28:26 +0000

    Match up some of the main plugin structure with wpforms

--- musicdistro.php
+++ musicdistro.php
@@ -10,7 +10,7 @@
  * Plugin Name: MusicDistro
  * Plugin URI:  https://jordanpak.com
  * Description: Enables bands to distribute sheet music and other tools to its members!
- * Version:     0.1.0
+ * Version:     1.0.0
  * Author:      Jordan Pakrosnis
  * Author URI:  https://jordanpak.com
  * Text Domain: musicdistro
@@ -23,20 +23,31 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * Main MusicDistro class.
  *
- * @since 0.1.0
+ * @since 1.0.0
  */
-class MusicDistro {
+final class MusicDistro {
 
 	/**
-	 * @var MusicDistro the instance of MusicDistro
-	 * @since 0.1.0
+	 * The one and only instance of MusicDistro
+	 *
+	 * @var object
+	 * @since 1.0.0
 	 */
 	private static $instance;
 
 
 	/**
+	 * Plugin version for enqueueing, etc.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+
+	/**
 	 * @var object|MD_Roles
-	 * @since 0.1.0
+	 * @since 1.0.0
 	 */
 	public $roles;
 
@@ -45,38 +56,22 @@ class MusicDistro {
 	/**
 	 * Main MusicDistro instance.
 	 *
-	 * @since 0.1.0
-	 * @static
-	 * @see MusicDistro()
-	 * @return object|MusicDistro the instance of MusicDistro
+	 * @since 1.0.0
+	 * @return MusicDistro
 	 */
 	public static function instance() {
 
 		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof MusicDistro ) ) {
 
 			self::$instance = new MusicDistro;
-			self::$instance->setup_constants();
+			self::$instance->constants();
 			self::$instance->includes();
 			// self::$instance->roles = new MD_Roles();
+		
+			add_action( 'plugins_loaded', array( self::$instance, 'objects' ), 10 );
 		}
 
 		return self::$instance;
-	}
-
-
-
-	/**
-	 * Throw error on object clone.
-	 *
-	 * The whole idea of the singleton design pattern is that there is a single
-	 * object therefore, we don't want the object to be cloned.
-	 *
-	 * @since 0.1.0
-	 * @access protected
-	 * @return void
-	 */
-	public function __clone() {
-		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'musicdistro' ), '0.1.0' );
 	}
 
 
@@ -84,22 +79,29 @@ class MusicDistro {
 	/**
 	 * Setup plugin constants.
 	 *
-	 * @since 0.1.0
-	 * @return void
+	 * @since 1.0.0
 	 */
-	private function setup_constants() {
+	private function constants() {
 
 		// plugin version
-		define( 'MD_VERSION', '0.1.0' );
+		if ( ! defined( 'MD_VERSION' ) ) {
+			define( 'MD_VERSION', $this->version );
+		}
 
 		// plugin folder path
-		define( 'MD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+		if ( ! defined( 'MD_PLUGIN_DIR' ) ) {
+			define( 'MD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+		}
 
 		// plugin folder URL
-		define( 'MD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+		if ( ! defined( 'MD_PLUGIN_URL' ) ) {
+			define( 'MD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+		}
 
 		// plugin root file
-		define( 'MD_PLUGIN_FILE', __FILE__ );
+		if ( ! defined( 'MD_PLUGIN_FILE' ) ) {
+			define( 'MD_PLUGIN_FILE', __FILE__ );
+		}
 	}
 
 
