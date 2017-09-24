4488ace08ffa082bd3950667d6899a8aaaeedaf6
JordanPak <jpak@jordanpak.com>
2017-09-24 17:34:32 +0000

    Reorder band class methods

--- includes/class-band.php
+++ includes/class-band.php
@@ -37,6 +37,120 @@ class MusicDistro_Band_Handler {
 	}
 
 
+
+	/**
+	 * Registers band taxonomy
+	 *
+	 * @since 1.0.0
+	 */
+	public function register_taxonomy() {
+			
+		$labels = array(
+			'name'                       => _x( 'Bands', 'Taxonomy General Name', 'musicdistro' ),
+			'singular_name'              => _x( 'Band', 'Taxonomy Singular Name', 'musicdistro' ),
+			'menu_name'                  => __( 'Bands', 'musicdistro' ),
+			'all_items'                  => __( 'All Bands', 'musicdistro' ),
+			'parent_item'                => __( 'Band', 'musicdistro' ),
+			'parent_item_colon'          => __( 'Band:', 'musicdistro' ),
+			'new_item_name'              => __( 'New Band Name', 'musicdistro' ),
+			'add_new_item'               => __( 'Add New Band', 'musicdistro' ),
+			'edit_item'                  => __( 'Edit Band', 'musicdistro' ),
+			'update_item'                => __( 'Update Band', 'musicdistro' ),
+			'view_item'                  => __( 'View Band', 'musicdistro' ),
+			'separate_items_with_commas' => __( 'Separate bands with commas', 'musicdistro' ),
+			'add_or_remove_items'        => __( 'Add or remove bands', 'musicdistro' ),
+			'choose_from_most_used'      => __( 'Choose from the most used', 'musicdistro' ),
+			'popular_items'              => __( 'Popular Bands', 'musicdistro' ),
+			'search_items'               => __( 'Search Bands', 'musicdistro' ),
+			'not_found'                  => __( 'Not Found', 'musicdistro' ),
+			'no_terms'                   => __( 'No bands', 'musicdistro' ),
+			'items_list'                 => __( 'Bands list', 'musicdistro' ),
+			'items_list_navigation'      => __( 'Bands list navigation', 'musicdistro' ),
+		);
+
+		$capabilities = array(
+			'manage_terms'               => 'manage_categories',
+			'edit_terms'                 => 'manage_categories',
+			'delete_terms'               => 'manage_categories',
+			'assign_terms'               => 'edit_posts',
+		);
+
+		$args = array(
+			'labels'                     => $labels,
+			'hierarchical'               => false,
+			'public'                     => false,
+			'show_ui'                    => true,
+			'show_admin_column'          => true,
+			'show_in_nav_menus'          => false,
+			'show_tagcloud'              => false,
+			'rewrite'                    => false,
+			'capabilities'               => $capabilities,
+			'show_in_rest'               => true,
+		);
+
+		register_taxonomy(
+			$this->tax_slug,
+			array( MusicDistro()->arrangement->cpt_slug ),
+			$args
+		);
+	}
+
+
+
+	/**
+	 * Manage admin taxonomy columns
+	 *
+	 * Hide the "Description" column and rename "Count" to
+	 * "Arrangements"
+	 *
+	 * @param  array  $content  band tax columns
+	 * @return array  $content  adjusted tax columns
+	 *
+	 * @since 1.0.0
+	 */ 
+	 public function edit_taxonomy_columns( $content ) {
+		
+		// hide description
+		unset( $content['description']);
+
+		// rename "Count" to "Arrangements"
+		$content['posts'] = __( 'Arrangements', 'musicdistro' );
+
+		return $content;
+	}
+	
+
+
+	/**
+	 * Get some bands (parent items)
+	 * 
+	 * If a post ID is supplied, this returns an array of
+	 * SELECTED bands. Otherwise, ALL top-level terms (bands)
+	 * are returned, whether they children (instruments) or
+	 * not.
+	 *
+	 * @param  int    $post_id  arrangement (probably) post ID
+	 * @return array            All top-level bands, or bands already set to the post
+	 *
+	 * @since 1.0.0
+	 */
+	public function get_bands( $post_id = null ) {
+
+		// grab post's
+		if ( $post_id ) {
+			return wp_get_post_terms( $post_id, $this->tax_slug, array( 'fields' => 'ids' ) );
+		}
+		
+		// grab all
+		$bands = get_terms( array(
+			'taxonomy'		=> $this->tax_slug,
+			'hide_empty'	=> false,
+		));
+		return $bands ?: array();
+	}
+
+
+
 	/**
 	 * Add instruments field
 	 *
@@ -123,117 +237,5 @@ class MusicDistro_Band_Handler {
 		$instruments = array_map( 'intval', $_POST['md_instruments'] );
 
 		add_term_meta( $term_id, 'md_instruments', $instruments );
-	}
-
-
-
-	/**
-	 * Manage admin taxonomy columns
-	 *
-	 * Hide the "Description" column and rename "Count" to
-	 * "Arrangements"
-	 *
-	 * @param  array  $content  band tax columns
-	 * @return array  $content  adjusted tax columns
-	 *
-	 * @since 1.0.0
-	 */ 
-	public function edit_taxonomy_columns( $content ) {
-		
-		// hide description
-		unset( $content['description']);
-
-		// rename "Count" to "Arrangements"
-		$content['posts'] = __( 'Arrangements', 'musicdistro' );
-
-		return $content;
-	}
-
-
-	/**
-	 * Registers band taxonomy
-	 *
-	 * @since 1.0.0
-	 */
-	public function register_taxonomy() {
-			
-		$labels = array(
-			'name'                       => _x( 'Bands', 'Taxonomy General Name', 'musicdistro' ),
-			'singular_name'              => _x( 'Band', 'Taxonomy Singular Name', 'musicdistro' ),
-			'menu_name'                  => __( 'Bands', 'musicdistro' ),
-			'all_items'                  => __( 'All Bands', 'musicdistro' ),
-			'parent_item'                => __( 'Band', 'musicdistro' ),
-			'parent_item_colon'          => __( 'Band:', 'musicdistro' ),
-			'new_item_name'              => __( 'New Band Name', 'musicdistro' ),
-			'add_new_item'               => __( 'Add New Band', 'musicdistro' ),
-			'edit_item'                  => __( 'Edit Band', 'musicdistro' ),
-			'update_item'                => __( 'Update Band', 'musicdistro' ),
-			'view_item'                  => __( 'View Band', 'musicdistro' ),
-			'separate_items_with_commas' => __( 'Separate bands with commas', 'musicdistro' ),
-			'add_or_remove_items'        => __( 'Add or remove bands', 'musicdistro' ),
-			'choose_from_most_used'      => __( 'Choose from the most used', 'musicdistro' ),
-			'popular_items'              => __( 'Popular Bands', 'musicdistro' ),
-			'search_items'               => __( 'Search Bands', 'musicdistro' ),
-			'not_found'                  => __( 'Not Found', 'musicdistro' ),
-			'no_terms'                   => __( 'No bands', 'musicdistro' ),
-			'items_list'                 => __( 'Bands list', 'musicdistro' ),
-			'items_list_navigation'      => __( 'Bands list navigation', 'musicdistro' ),
-		);
-
-		$capabilities = array(
-			'manage_terms'               => 'manage_categories',
-			'edit_terms'                 => 'manage_categories',
-			'delete_terms'               => 'manage_categories',
-			'assign_terms'               => 'edit_posts',
-		);
-
-		$args = array(
-			'labels'                     => $labels,
-			'hierarchical'               => false,
-			'public'                     => false,
-			'show_ui'                    => true,
-			'show_admin_column'          => true,
-			'show_in_nav_menus'          => false,
-			'show_tagcloud'              => false,
-			'rewrite'                    => false,
-			'capabilities'               => $capabilities,
-			'show_in_rest'               => true,
-		);
-
-		register_taxonomy(
-			$this->tax_slug,
-			array( MusicDistro()->arrangement->cpt_slug ),
-			$args
-		);
-	}
-
-
-	
-	/**
-	 * Get some bands (parent items)
-	 * 
-	 * If a post ID is supplied, this returns an array of
-	 * SELECTED bands. Otherwise, ALL top-level terms (bands)
-	 * are returned, whether they children (instruments) or
-	 * not.
-	 *
-	 * @param  int    $post_id  arrangement (probably) post ID
-	 * @return array            All top-level bands, or bands already set to the post
-	 *
-	 * @since 1.0.0
-	 */
-	public function get_bands( $post_id = null ) {
-
-		// grab post's
-		if ( $post_id ) {
-			return wp_get_post_terms( $post_id, $this->tax_slug, array( 'fields' => 'ids' ) );
-		}
-		
-		// grab all
-		$bands = get_terms( array(
-			'taxonomy'		=> $this->tax_slug,
-			'hide_empty'	=> false,
-		));
-		return $bands ?: array();
 	}
 }
